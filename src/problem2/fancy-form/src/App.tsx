fe315a6e56808340d376205bc5c2bf51e120b589
minhtien3297 <minhitne@gmail.com>
2026-03-01 15:01:52 +0700

    [feat]: init problem 2 using vite, react, shadcn

---
+++ src/problem2/fancy-form/src/App.tsx
@@ -0,0 +1,16 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
