413eb6bf75bf5c6835e9d130b470ffdaf78e93e0
minhtien3297 <minhitne@gmail.com>
2026-03-01 19:49:22 +0700

    [feat]: add not found page

--- src/problem2/fancy-form/src/App.tsx
+++ src/problem2/fancy-form/src/App.tsx
@@ -3,12 +3,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Toaster } from "@/components/ui/sonner";
 import SwapTokenPage from '@/features/fancy-form/components/pages/swap-token'
+import NotFoundPage from '@/pages/NotFound'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SwapTokenPage/>} />
+        <Route path="/" element={<SwapTokenPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
       <Toaster />
---
+++ src/problem2/fancy-form/src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      Not Found Page
+    </div>
+  )
+}
